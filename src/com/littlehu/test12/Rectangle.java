765565fe7ebd119fe98bdf5d661460a76949cf3e
blackfox <huangy_tao@126.com>
2018-09-30 18:24:46 +0800

    装饰器模式

---
+++ src/com/littlehu/test12/Rectangle.java
@@ -0,0 +1,12 @@
+package com.littlehu.test12;
+
+public class Rectangle implements Shape{
+
+	@Override
+	public void draw() {
+		
+		System.out.println("Shape: Rectangle");
+		
+	}
+
+}
---
+++ src/com/littlehu/test12/Shape.java
@@ -0,0 +1,7 @@
+package com.littlehu.test12;
+
+public interface Shape {
+	
+	void draw();
+
+}
