44f50f2e6a202b32d1167dc6904949d2dc70e2d5
blackfox <huangy_tao@126.com>
2018-10-16 17:27:44 +0800

    命令模式

---
+++ src/com/littlehu/test17/BuyStock.java
@@ -0,0 +1,16 @@
+package com.littlehu.test17;
+
+public class BuyStock implements Order{
+	
+	private Stock abcStock;
+
+	public BuyStock(Stock abcStock) {
+		this.abcStock=abcStock;
+	}
+	@Override
+	public void execute() {
+		abcStock.buy();
+		
+	}
+
+}
---
+++ src/com/littlehu/test17/Order.java
@@ -0,0 +1,6 @@
+package com.littlehu.test17;
+
+public interface Order {
+	void execute();
+
+}
