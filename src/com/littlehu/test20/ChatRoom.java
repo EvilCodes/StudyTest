6a36ef36d28669178a876a215b8c1011f6931291
blackfox <huangy_tao@126.com>
2018-10-19 17:35:47 +0800

    中介者模式

---
+++ src/com/littlehu/test20/ChatRoom.java
@@ -0,0 +1,13 @@
+package com.littlehu.test20;
+
+import java.util.Date;
+
+public class ChatRoom {
+	public static void showMessage(User user,String message){
+		
+		
+		System.out.println(new Date().toString()+"["+user.getName()+"]:"+message);
+	}
+	
+
+}
